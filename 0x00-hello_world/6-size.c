ba5d6eb17e58377e58e36e773dbba14f7cd61904
Lewis-mbui <lewmugo14479@gmail.com>
2023-08-10 15:24:07 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int li;
 long long int lli;
 float f;
 
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of an int: %ld byte(s)\n", sizeof(i));
-printf("size of a long int: %ld byte(s)\n", sizeof(li));
-printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
-printf("size of a float: %ld byte(s)\n", sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(c));
+printf("Size of an int: %zu byte(s)\n", sizeof(i));
+printf("size of a long int: %zu byte(s)\n", sizeof(li));
+printf("size of a long long int: %zu byte(s)\n", sizeof(lli));
+printf("size of a float: %zu byte(s)\n", sizeof(f));
 return (0);
 }
