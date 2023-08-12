ebdea83c6193593e89c62216fee5ea0062a8e869
Lewis-mbui <lewmugo14479@gmail.com>
2023-08-11 18:16:14 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints the whole alphabet using putchar
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
