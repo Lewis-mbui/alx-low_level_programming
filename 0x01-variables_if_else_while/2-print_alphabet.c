467a0366574c5d8e6730783bf0a75e8e075252cf
Lewis-mbui <lewmugo14479@gmail.com>
2023-08-11 17:38:02 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints the whole alphabet using putchar
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
