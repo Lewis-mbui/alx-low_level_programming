a0fa7c2618eb4723181b3fb18c83634c1b5362b2
Lewis-mbui <lewmugo14479@gmail.com>
2023-08-11 19:01:47 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 1666666)
+	{
+		printf("%x", a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
