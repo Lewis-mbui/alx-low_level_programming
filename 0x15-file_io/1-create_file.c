36aa98aa1f1dab19ffe92caf5cef1047fec7ba92
Lewis-mbui <lewmugo14479@gmail.com>
2023-10-02 13:44:17 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	written = write(fd, text_content, len);
+	written = write(file_descriptor, text_content, size);
 
 	if (file_descriptor == -1 || written == -1)
 		return (-1);
