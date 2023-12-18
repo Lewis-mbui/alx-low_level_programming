77bf900cca26293bef9907205476b760b68b19f3
Lewis-mbui <lewmugo14479@gmail.com>
2023-12-18 05:41:53 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,3 +25,74 @@ char *allocateBuffer(char *targetFile)
 	}
 	return (buffer);
 }
+
+/**
+ * closeFile - closes files
+ * @fileDescriptor: file to be closed
+ */
+
+void closeFile(int fileDescriptor)
+{
+	int result;
+
+	result = close(fileDescriptor);
+
+	if (result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close"
+				"file descriptor %d\n", fileDescriptor);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies contents of a file to another file
+ * @argc: number of arguments
+ * @argv: array of the arguments supplied
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int sourceFile, destinationFile, bytesRead, bytesWritten;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp source_file destination_file\n");
+		exit(97);
+	}
+	buffer = allocateBuffer(argv[2]);
+	sourceFile = open(argv[1], O_RDONLY);
+	bytesRead = read(sourceFile, buffer, 1024);
+	destinationFile = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (sourceFile == -1 || bytesRead == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		bytesWritten = write(destinationFile, buffer, bytesRead);
+		if (destinationFile == -1 || bytesWritten == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		bytesRead = read(sourceFile, buffer, 1024);
+		destinationFile = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (bytesRead > 0);
+
+	free(buffer);
+	closeFile(sourceFile);
+	closeFile(destinationFile);
+
+	return (0);
+}
