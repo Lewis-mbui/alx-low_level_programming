e77b61716fe95f8f27f1de2abb156a62c8bc1d49
Lewis-mbui <lewmugo14479@gmail.com>
2023-12-18 05:35:44 -0800

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write into the file
+ * Return: If the function fails 1 otherwise - 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor, written, size = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (size = 0; text_content[size];)
+			size++;
+	}
+
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	written = write(file_descriptor, text_content, size);
+
+	if (file_descriptor == -1 || written == -1)
+		return (-1);
+
+	close(file_descriptor);
+
+	return (1);
+}
